5f006401d01115ba32dc19a875669a6ddd210f96
Vasant Palisetti <vpalisetti@gmail.com>
2015-08-23 21:33:08 -0700

    updated required changes for inverse matrix checks

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        #m <- NULL
+        ix <- matrix()
+        set <- function(y) {
+                x <<- y
+                #m <<- NULL
+                ix <- matrix()
+        }
+        get <- function() x
+        setinverse <- function(solve) ix <<- solve
+        getinverse <- function() ix
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ix <- x$getinverse()
+        if(!is.na(ix)) {
+                message("getting cached data")
+                return(ix)
+        }
+        data <- x$get()
+        ix <- solve(data, ...)
+        x$setinverse(ix)
+        ix
 }
